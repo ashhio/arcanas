d4ca8710359400a8535a9d01e9edc0fe6f79d07f
Akshaya Madhavan <akshayam@Akshayas-MacBook-Air.local>
2018-08-13 20:06:28 -0500

    cleaned some css. started adding descriptions to cards on results page.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
- function Redirect() {
-               window.location="result.html";
-            }
+var cardNumber;
 
-function generate()
-{
-	var randomNumber = Math.floor(Math.random() * 3);
-	var imgName = "card_" + randomNumber + ".png";
+var card_info = ["Beginnings, Spontaneity, Faith, Apparent folly", 
+				"Action, Conscious awareness, Concentration, Power",
+				"Nonaction, Unconscious awareness, Potential, Mystery"]
+
+function Redirect() {
+	window.location="result.html";
+}
+
+function generateCard() {
+	cardNumber = Math.floor(Math.random() * 3);
+	var imgName = "card_" + cardNumber + ".png";
 	document.getElementById("generated-card").src= "images" + "/" + imgName;
 }
 
-function generateCard()
-{
-	window.onload = generate();
-}
+function showCardInfo() {
+	var para = document.createElement("p");
+	var node = document.createTextNode(card_info[cardNumber]);
+	para.appendChild(node);
 
+	var element = document.getElementById("card-info");
+	element.appendChild(para);
+}
