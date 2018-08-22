68196e7eef6838ad0d609d73a921ae05a9492b0d
Akshaya Madhavan <akshayam@Akshayas-MacBook-Air.local>
2018-08-21 20:13:40 -0500

    started polishing and adding text content to results page.

--- js/script.js
+++ js/script.js
@@ -2,14 +2,16 @@ var cardNumber;
 
 var card_info = ["Beginnings, Spontaneity, Faith, Apparent folly", 
 				"Action, Conscious awareness, Concentration, Power",
-				"Nonaction, Unconscious awareness, Potential, Mystery"]
+				"Nonaction, Unconscious awareness, Potential, Mystery",
+				"Mothering, Abundance, Senses, Nature",
+				"Fathering, Structure, Authority, Regulation"]
 
 function Redirect() {
 	window.location="result.html";
 }
 
 function generateCard() {
-	cardNumber = Math.floor(Math.random() * 3);
+	cardNumber = Math.floor(Math.random() * 5);
 	var imgName = "card_" + cardNumber + ".png";
 	document.getElementById("generated-card").src= "images" + "/" + imgName;
 }
