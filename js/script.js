5974d8eefbe8f3085a90d7a35b48cd24e0698083
Akshaya Madhavan <akshayam@Akshayas-MacBook-Air.local>
2018-08-24 14:59:36 -0500

    finished results page layout.

--- js/script.js
+++ js/script.js
@@ -4,14 +4,24 @@ var card_info = ["Beginnings, Spontaneity, Faith, Apparent folly",
 				"Action, Conscious awareness, Concentration, Power",
 				"Nonaction, Unconscious awareness, Potential, Mystery",
 				"Mothering, Abundance, Senses, Nature",
-				"Fathering, Structure, Authority, Regulation"]
+				"Fathering, Structure, Authority, Regulation",
+				"Education, Belief systems, Conformity, Group identification",
+				"Relationship, Sexuality, Personal beliefs, Values"]
+
+var card_name = ["The Fool",
+				"The Magician",
+				"The High Priestess",
+				"The Empress",
+				"The Emperor",
+				"The Hierophant",
+				"The Lovers"]
 
 function Redirect() {
 	window.location="result.html";
 }
 
 function generateCard() {
-	cardNumber = Math.floor(Math.random() * 5);
+	cardNumber = Math.floor(Math.random() * 7);
 	var imgName = "card_" + cardNumber + ".png";
 	document.getElementById("generated-card").src= "images" + "/" + imgName;
 }
@@ -22,5 +32,14 @@ function showCardInfo() {
 	para.appendChild(node);
 
 	var element = document.getElementById("card-info");
+	element.appendChild(para);
+}
+
+function showCardName() {
+	var para = document.createElement("p");
+	var node = document.createTextNode(card_name[cardNumber]);
+	para.appendChild(node);
+
+	var element = document.getElementById("card-name");
 	element.appendChild(para);
 }
